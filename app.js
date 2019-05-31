ad7fc6e6cd79bd0120aa20fb5d1e02f96f6f1538
Kim Jun Sik <jinaganda5@gmail.com>
2019-05-31 20:20:20 +0900

    MVC Pattern part Two

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
 
 const app = express();
 
@@ -16,8 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/users", userRouter);
-app.use("/videos", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,5 +1,22 @@
 import express from "express";
+import routes from "../routes";
 
 const globalRouter = express.Router();
+
+globalRouter.get(routes.home, (req, res) => {
+  res.send("here, Home");
+});
+globalRouter.get(routes.join, (req, res) => {
+  res.send("here, Join");
+});
+globalRouter.get(routes.login, (req, res) => {
+  res.send("here, Login");
+});
+globalRouter.get(routes.logout, (req, res) => {
+  res.send("here, Logout");
+});
+globalRouter.get(routes.search, (req, res) => {
+  res.send("here, Search");
+});
 
 export default globalRouter;
