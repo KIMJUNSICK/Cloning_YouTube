503ac7b3f9f80b0a34f6e8863aca73daedca3392
Kim Jun Sik <jinaganda5@gmail.com>
2019-06-03 14:10:25 +0900

    More Controlers

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,16 +1,23 @@
 import express from "express";
 import routes from "../routes";
 import { home, serach } from "../controller/videoController";
-import { postJoin, getJoin, login, logout } from "../controller/userController";
+import {
+  postJoin,
+  getJoin,
+  postLogin,
+  getLogin,
+  logout
+} from "../controller/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, serach);
-globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
