36f748073fabecea81235cdcab4a77704c90f207
Kim Jun Sik <jinaganda5@gmail.com>
2019-06-04 18:10:57 +0900

    Connecting to MongoDB

---
+++ db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/weTube", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("😊  Connected to DB");
+const handleError = error => console.log(`Error on DB Connection:${error}`);
+db.once("open", handleOpen);
+db.on("error", handleError);
