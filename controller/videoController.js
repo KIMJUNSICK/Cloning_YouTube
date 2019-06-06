c6ffce633e820e081786601583372aa1baebeec9
Kim Jun Sik <jinaganda5@gmail.com>
2019-06-06 09:32:24 +0900

    Getting Video by ID

--- controller/videoController.js
+++ controller/videoController.js
@@ -31,12 +31,22 @@ export const postUpLoad = async (req, res) => {
     title,
     description
   });
-  console.log(newVideo);
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const videoDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle: "Video Detail", video });
+  } catch (error) {
+    console.log(error);
+    res.redirect(route.home);
+  }
+};
+
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) =>
