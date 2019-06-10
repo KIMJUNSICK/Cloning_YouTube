039bc458d3df56539967ec0fc02d341cb0560c62
Kim Jun Sik <jinaganda5@gmail.com>
2019-06-10 09:40:56 +0900

    Local Authentication with Passport part Two

--- passport.js
+++ passport.js
@@ -2,3 +2,6 @@ import passport from "passport";
 import User from "./models/User";
 
 passport.use(User.createStategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
