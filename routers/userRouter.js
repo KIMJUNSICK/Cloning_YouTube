22862553971bd570cabced42667bc0773919cd64
Kim Jun Sik <jinaganda5@gmail.com>
2019-06-14 08:56:05 +0900

    Try to solve Erorr

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,7 +2,7 @@ import express from "express";
 import routes from "../routes";
 import {
   userDetail,
-  editProfile,
+  getEditProfile,
   changePassword
 } from "../controller/userController";
 import { onlyPrivate } from "../middlewares";
@@ -10,7 +10,7 @@ import { onlyPrivate } from "../middlewares";
 const userRouter = express.Router();
 
 userRouter.get(routes.userDetail(), userDetail);
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 
 export default userRouter;
