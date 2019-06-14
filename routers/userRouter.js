33f6516d07123f290813c018055d62c01cd284d2
Kim Jun Sik <jinaganda5@gmail.com>
2019-06-14 09:19:52 +0900

    Solve the Error!

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,8 +9,8 @@ import { onlyPrivate } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.userDetail(), userDetail);
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
+userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
