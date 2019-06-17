e0120801fd28efafce7f817e5d6b9bf7fc548ff0
Kim Jun Sik <jinaganda5@gmail.com>
2019-06-17 12:56:31 +0900

    Recording Video
    Recording Video with JavaScript

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -1,24 +1,55 @@
 const recodrContainer = document.getElementById("jsRecordContainer");
-const videoRecorder = document.getElementById("jsVideoPreview");
+const videoPreview = document.getElementById("jsVideoPreview");
 const recordBtn = document.getElementById("jsRecordBtn");
 
-const startRecording = async () => {
+let streamObject;
+let videoRecorder;
+
+const handleVideoData = event => {
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerText = "Start Recording";
+};
+
+const startRecording = () => {
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  console.log(videoRecorder);
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true
       //   video: true
     });
-    videoRecorder.srcObject = stream;
-    videoRecorder.muted = true;
-    videoRecorder.play();
+    videoPreview.srcObject = stream;
+    videoPreview.muted = true;
+    videoPreview.play();
+    streamObject = stream;
+    recordBtn.innerText = "Stop Recording";
+    startRecording();
   } catch (error) {
     recordBtn.innerText = "Can't Record 💔";
-    recordBtn.removeEventListener("click", startRecording);
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
 const init = () => {
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 };
 
 if (recodrContainer) {
